d7583285dc74f54bed7aee7a330d994e9e2cbfbc
kazimsanlav <kazimsanlav@gmail.com>
2018-09-06 15:55:01 +0300

    Added project plan

--- sketch.js
+++ sketch.js
@@ -1,14 +1,4 @@
-// The Nature of Code
-// Daniel Shiffman
-// http://natureofcode.com
 
-// Seeking "vehicle" follows the mouse position
-
-// Implements Craig Reynold's autonomous steering behaviors
-// One vehicle "seeks"
-// See: http://www.red3d.com/cwr/
-
-// let v, target;
 let simulation;
 
 
